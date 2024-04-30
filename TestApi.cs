d168eb77e79c7a097ecc3dd56f803cd6ccb94cb8
Hanson Chen <hexun_chen@hotmail.com>
2024-04-30 12:51:45 -0400

    initial version

---
+++ TestApi.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Techbuild.Function
+{
+    public class TestApi
+    {
+        private readonly ILogger<HttpTriggerFunc> _logger;
+
+        public TestApi(ILogger<HttpTriggerFunc> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("TestApi")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            Person p = new Person()
+            {
+                Name="Hanson",
+                Address="Toronto"
+            };
+            return new OkObjectResult(p);
+        }
+    }
+
+    public class Person{
+        public string Name { get; set;}
+        public string Address { get; set;}
+
+    }
+}
